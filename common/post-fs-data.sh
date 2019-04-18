6bf9bacc1c811db7f12f09596158cb402493be22
Marius Kopp <mariuskopp517@gmail.com>
2019-04-18 03:08:36 +0200

    Update
    Trying to get Google Dialer / Call Screening to work. Test 2

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,2 +1,20 @@
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+
+XML_FILE=/data/data/com.google.android.dialer/shared_prefs/dialer_phenotype_flags.xml
+
+if [ -f $XML_FILE ]; then
+
+  sed -i -e 's/name="G__speak_easy_bypass_locale_check" value="false"/name="G__speak_easy_bypass_locale_check" value="true"/g' $XML_FILE
+  sed -i -e 's/name="G__speak_easy_enable_listen_in_button" value="false"/name="G__speak_easy_enable_listen_in_button" value="true"/g' $XML_FILE
+  sed -i -e 's/name="__data_rollout__SpeakEasy.OverrideUSLocaleCheckRollout__launched__" value="false"/name="__data_rollout__SpeakEasy.OverrideUSLocaleCheckRollout__launched__" value="true"/g' $XML_FILE
+  sed -i -e 's/name="G__enable_speakeasy_details" value="false"/name="G__enable_speakeasy_details" value="true"/g' $XML_FILE
+  sed -i -e 's/name="G__speak_easy_enabled" value="false"/name="G__speak_easy_enabled" value="true"/g' $XML_FILE
+  sed -i -e 's/name="G__speakeasy_show_privacy_tour" value="false"/name="G__speakeasy_show_privacy_tour" value="true"/g' $XML_FILE
+  sed -i -e 's/name="__data_rollout__SpeakEasy.SpeakEasyDetailsRollout__launched__" value="false"/name="__data_rollout__SpeakEasy.SpeakEasyDetailsRollout__launched__" value="true"/g' $XML_FILE
+  sed -i -e 's/name="__data_rollout__SpeakEasy.CallScreenOnPixelTwoRollout__launched__" value="false"/name="__data_rollout__SpeakEasy.CallScreenOnPixelTwoRollout__launched__" value="true"/g' $XML_FILE
+
+  pm grant "com.google.android.dialer" android.permission.WRITE_EXTERNAL_STORAGE
+  am force-stop "com.google.android.dialer"
+
+fi
